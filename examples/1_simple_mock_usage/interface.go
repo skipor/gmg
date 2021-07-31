222665aaa06041d646249137788018bf6a22e8e9
Vladimir Skipor <v@skipor.com>
2021-07-31 13:49:38 +0300

    interface selection examples

--- examples/1_simple_mock_usage/interface.go
+++ examples/1_simple_mock_usage/interface.go
@@ -1,7 +1,10 @@
 package simple_mock_usage
 
+// `//go:generate gmg` without interface names arguments generates mock for next type declaration.
+// That is, that comment generates mock for Foo.
 //go:generate gmg
 
+// Foo is an example interface.
 type Foo interface {
 	Bar(s string) error
 }
---
+++ examples/2_target_interface_select/select.go
@@ -0,0 +1,33 @@
+package target_interface_selection
+
+import (
+	"go.uber.org/zap/zapcore"
+)
+
+// `//go:generate gmg`  without interface names arguments generates mock for next type declaration.
+// That is, that comment generates mock for Foo.
+//go:generate gmg
+
+// Foo is an example interface.
+type Foo interface {
+	Bar(s string) error
+}
+
+// One or more interface names can be passed as arguments.
+//go:generate gmg First Second Third
+
+type (
+	First  interface{ One() }
+	Second interface{ Two() }
+	Third  interface{ Three() }
+)
+
+// Absolute or relative source package can be specified via `--src (-s)` flag
+//go:generate gmg --src ./sub Baz
+//go:generate gmg --src io Reader Writer Closer
+//go:generate gmg --src go.uber.org/zap/zapcore Core
+
+// Instead of specifying full source package path, mock can be generated for type alias.
+//go:generate gmg
+
+type ZapEncoder = zapcore.Encoder
