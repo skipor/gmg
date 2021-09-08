33001a97113668530b49c769ad76e8526621fd48
Vladimir Skipor <v@skipor.com>
2021-09-08 10:17:03 +0300

    more go gen tests

--- test/multi_select_test.go
+++ test/multi_select_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGoGenerate_All_Primary(t *testing.T) {
 	tr := newTester(t, M{
-		Name: "pkg",
+		Name: "repo/pkg",
 		Files: map[string]interface{}{
 			"primary.go": /* language=go */ `
 			package pkg
@@ -26,30 +26,64 @@ func TestGoGenerate_All_Primary(t *testing.T) {
 			`,
 		},
 	})
-	tr.GoGenerate(t).Succeed().Files("mocks/primary_1.go", "mocks/primary_2.go")
+	tr.GoGenerate(t).Succeed().Files("mocks/primary_1.go", "mocks/primary_2.go").
+		Golden()
 }
 
-//func TestGoGenerate_All_Test(t *testing.T) {
-//	tr := newTester(t, M{
-//		Name: "pkg",
-//		Files: map[string]interface{}{
-//			"primary.go": /* language=go */ `
-//			package pkg
-//			type Primary1 interface { P1()  }
-//			type Primary2 interface { P2()  }
-//			`,
-//			"primary_test.go": /* language=go */ `
-//			package pkg
-//			//go:generate gmg --all
-//			type Test1 interface { T1()  }
-//			type Test2 interface { T2()  }
-//			`,
-//			"black_box_test.go": /* language=go */ `
-//			package pkg_test
-//			type BlackBox1 interface { BB1()  }
-//			type BlackBox2 interface { BB2()  }
-//			`,
-//		},
-//	})
-//	tr.GoGenerate(t).Succeed().Files("test1.go", "test2.go")
-//}
+func TestGoGenerate_All_Test(t *testing.T) {
+	tr := newTester(t, M{
+		Name: "repo/pkg",
+		Files: map[string]interface{}{
+			"primary.go": /* language=go */ `
+			package pkg
+			type Primary1 interface { P1()  }
+			type Primary2 interface { P2()  }
+			`,
+			"primary_test.go": /* language=go */ `
+			package pkg
+			//go:generate gmg --all
+			type Test1 interface { T1()  }
+			type Test2 interface { T2()  }
+			`,
+			"black_box_test.go": /* language=go */ `
+			package pkg_test
+			type BlackBox1 interface { BB1()  }
+			type BlackBox2 interface { BB2()  }
+			`,
+		},
+	})
+	tr.GoGenerate(t).
+		Succeed().
+		// TODO(skipor): change default dst: test_1_test.go, test_2_test.go
+		Files("mocks/test_1.go", "mocks/test_2.go").
+		Golden()
+}
+
+func TestGoGenerate_All_BlackBoxTest(t *testing.T) {
+	tr := newTester(t, M{
+		Name: "repo/pkg",
+		Files: map[string]interface{}{
+			"primary.go": /* language=go */ `
+			package pkg
+			type Primary1 interface { P1()  }
+			type Primary2 interface { P2()  }
+			`,
+			"primary_test.go": /* language=go */ `
+			package pkg
+			type Test1 interface { T1()  }
+			type Test2 interface { T2()  }
+			`,
+			"black_box_test.go": /* language=go */ `
+			//go:generate gmg --all
+			package pkg_test
+			type BlackBox1 interface { BB1()  }
+			type BlackBox2 interface { BB2()  }
+			`,
+		},
+	})
+	tr.GoGenerate(t).
+		Succeed().
+		// TODO(skipor): change default dst: test_1_test.go, test_2_test.go
+		Files("mocks/black_box_1.go", "mocks/black_box_2.go").
+		Golden()
+}
